e668fabc6a0c43623e5545b69713def7d2af2487
woowon123 <wlsdndnjs12@naver.com>
2021-04-14 13:22:54 +0900

    Creata Random int( No duplication)
    Creata Random int( No duplication)
    중복 없는 랜덤 int 생성 c#

---
+++ C#_Unity/RandomInt.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class RandomInt 
+{
+    public static List<int> getRandomInt(int length, int min, int max)
+    {
+        List<int> randArray = new List<int>();
+
+        while (true)
+        {
+            if (randArray.Count == length)
+                break;
+
+            randArray.Add(Random.Range(min, max));
+            randArray = randArray.Distinct().ToList();
+        }
+
+        return randArray;
+    }
+}
